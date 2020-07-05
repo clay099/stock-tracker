e505d335b0913b826feb48357c5f1a718dd525c1
Clayton Whittaker <Clayton@Madelines-MacBook-Air.local>
2020-07-05 13:26:54 -0700

    delete modal showing

--- static/script.js
+++ static/script.js
@@ -1,8 +1,28 @@
 $(".table").on("click", "tr", function (evt) {
-	console.log(evt);
-	// show edit form modal
-	$("#edit-stock").modal("show");
-	// change input value to the stock symbol of row selected. This input value is read only and the user cannot change this value
-	$("input[name=stock_symbol]").val(evt.currentTarget.id);
-	$("input[name=stock_num]").val($(this).find("td.stock_num").text());
+    // show edit form modal
+    console.log(evt);
+    if (evt.target.classList.contains("fas")) {
+        return;
+    } else {
+        $("#edit-stock").modal("show");
+        // change input value to the stock symbol of row selected. This input value is read only and the user cannot change this value
+        $("input[name=stock_symbol]").val(evt.currentTarget.id);
+        $("input[name=stock_num]").val($(this).find("td.stock_num").text());
+    }
+});
+
+$(".trash").hover(
+    function () {
+        $(".trash").toggleClass("far fas");
+    },
+    function () {
+        $(".trash").toggleClass("far fas");
+    }
+);
+
+$(".table").on("click", ".fas", function (evt) {
+    // show edit form modal
+    $("#delete-stock").modal("show");
+    $("#symbol").text(evt.target.closest("tr").id);
+    $("input[name=stock_symbol]").val(evt.target.closest("tr").id);
 });
