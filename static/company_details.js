b80e10ae73a11aae21018cdd82ff9750fe932f9d
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-07-09 15:22:48 -0700

    news cards rendering

--- static/company_details.js
+++ static/company_details.js
@@ -11,6 +11,27 @@ async function basicCompanyDetails() {
 async function companyNews() {
 	let stock_symbol = $("h1").attr("id");
 	const returnedDetails = await axios.post(`${BASE_URL}/company-details/news`, { stock_symbol });
+	for (let article of returnedDetails.data.news) {
+		let newArticle = $(generateNewsHTML(article));
+		$("#articles").append(newArticle);
+	}
+}
+
+function generateNewsHTML(newsArticle) {
+	return `
+    <div class="col mb-4">
+        <div class="card" style="width: 18rem;">
+            <img src="${newsArticle.image}" class="card-img-top" alt="Image failed to load">
+            <div class="card-body">
+                <h5 class="card-title">${newsArticle.headline}</h5>
+                <p class="card-text">
+                    ${newsArticle.summary}
+                </p>
+                <a href="${newsArticle.url}" class="card-link">See Full Article</a>
+            </div>
+        </div>
+    </div>
+    `;
 }
 
 basicCompanyDetails();
