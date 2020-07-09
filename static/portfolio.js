b2aae230fb122843d1482099c954e9690b9bae85
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-07-08 17:37:06 -0700

    update edit stock to select current target

--- static/portfolio.js
+++ static/portfolio.js
@@ -56,8 +56,8 @@ $("#portfolio-table").on("click", "th", function (evt) {
 	} else {
 		$("#edit-stock").modal("show");
 		// change input value to the stock symbol of row selected. This input value is read only and the user cannot change this value
-		$("input[name=stock_symbol]").val(evt.currentTarget.id);
-		$("input[name=stock_num]").val($(this).find("td.stock_num").text());
+		$("#edit-stock").find("input[name=stock_symbol]").val(evt.currentTarget.textContent);
+		$("#edit-stock").find("input[name=stock_num]").val($(this).find("td.stock_num").text());
 	}
 });
 
